788872433f5da8345144f585eaea1796bf32307f
kwugfighter <isaacli430@gmail.com>
2018-07-13 20:57:28 +0800

    a

--- bot.py
+++ bot.py
@@ -44,6 +44,12 @@ class BaiterBot(commands.Bot):
         perms.administrator = True
         print(f"Bot is ready! Invite: {discord.utils.oauth_url(self.user.id, perms)}")
 
+    async def on_member_join(self, member):
+        await discord.utils.get(member.guild.text_channels, name="welcome").send(f"Hey {member.mention}, welcome to Masters Of Baiting! Please read the #rules. Suggestions are always welcome in #feedback too. Enjoy your stay here!\n\nInvite link: https://discord.gg/MtpjRff")
+
+    async def on_member_remove(self, member):
+        await discord.utils.get(member.guild.text_channels, name="welcome").send(f"**{member.name}** just left {member.guild.name}. Bye! 👋")
+
     async def on_command_error(self, ctx, error):
         if isinstance(error, commands.errors.CheckFailure):
             return await ctx.send("You don't have the permissions to run that command!")
